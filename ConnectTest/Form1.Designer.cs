a81e3a8b4cdb64295001f5a89edd829d0f134c09
namutree0345 <redwikitv@gmail.com>
2020-07-07 11:39:45 +0900

    First commit, 0.2

---
+++ ConnectTest/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ConnectTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ver = new System.Windows.Forms.Label();
+            this.hour = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minute = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.site = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ver
+            // 
+            this.ver.AutoSize = true;
+            this.ver.Location = new System.Drawing.Point(10, 138);
+            this.ver.Name = "ver";
+            this.ver.Size = new System.Drawing.Size(198, 12);
+            this.ver.TabIndex = 0;
+            this.ver.Text = "Connect Tester by NamuTree0345";
+            // 
+            // hour
+            // 
+            this.hour.Location = new System.Drawing.Point(14, 12);
+            this.hour.Name = "hour";
+            this.hour.Size = new System.Drawing.Size(47, 21);
+            this.hour.TabIndex = 1;
+            this.hour.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "시간";
+            // 
+            // minute
+            // 
+            this.minute.Location = new System.Drawing.Point(102, 12);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(47, 21);
+            this.minute.TabIndex = 1;
+            this.minute.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "분";
+            // 
+            // second
+            // 
+            this.second.Location = new System.Drawing.Point(190, 12);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(47, 21);
+            this.second.TabIndex = 1;
+            this.second.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "초";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "마다 확인";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "* 시간이 짧을수록 CPU 사용률이 늘어납니다\r\n* 약간의 딜레이가 있습니다";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "시작/설정 저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // site
+            // 
+            this.site.Location = new System.Drawing.Point(57, 51);
+            this.site.Name = "site";
+            this.site.Size = new System.Drawing.Size(262, 21);
+            this.site.TabIndex = 1;
+            this.site.Text = "http://example.com";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "사이트";
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Tick += new System.EventHandler(this.checkTimer_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "설정 초기화";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 159);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.minute);
+            this.Controls.Add(this.site);
+            this.Controls.Add(this.hour);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ver);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "연결 테스트기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ver;
+        private System.Windows.Forms.TextBox hour;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox minute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox second;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox site;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer checkTimer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ConnectTest/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectTest
+{
+    public partial class Form1 : Form
+    {
+
+        public static bool isRunning = false;
+        public static string version = "0.2";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ConnectTest_Conf.txt")) {
+                string txt = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ConnectTest_Conf.txt");
+
+                hour.Text = txt.Split(' ')[0];
+                minute.Text = txt.Split(' ')[1];
+                second.Text = txt.Split(' ')[2];
+                site.Text = txt.Split(' ')[3];
+            }
+
+            ver.Text = string.Format("Connect Tester v{0} by NamuTree0345", version);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (isRunning) { checkTimer.Stop();  isRunning = false; button1.Text = "시작/설정 저장"; } else {
+                try
+                {
+                    
+                    checkTimer.Interval = getMs(int.Parse(hour.Text), int.Parse(minute.Text), int.Parse(second.Text));
+                    checkTimer.Start();
+                    isRunning = true;
+                    button1.Text = "종료";
+                    File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ConnectTest_Conf.txt", hour.Text + " " + minute.Text + " " + second.Text + " " + site.Text);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("시/분/초는 정수로 적어야 됩니다.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (IOException ex) {
+                    MessageBox.Show("설정을 저장하는데 오류가 났습니다. 메시지: " + ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public static int getMs(int hour, int minute, int seconds) {
+            return (seconds * 1000) + (minute * 60000) + (hour * 600000);
+        }
+
+        private void checkTimer_Tick(object sender, EventArgs e)
+        {
+            notifyIcon.BalloonTipTitle = "HTTP 연결 확인";
+            notifyIcon.BalloonTipText = "연결 상태 확인중...";
+            notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(site.Text);
+                req.Method = "GET";
+                using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+                {
+                    notifyIcon.BalloonTipText = "연결 상태: " + resp.StatusCode;
+                    notifyIcon.ShowBalloonTip(3000);
+                }
+            } catch {
+                notifyIcon.BalloonTipIcon = ToolTipIcon.Error;
+                notifyIcon.BalloonTipText = "연결 상태: 오류!";
+                notifyIcon.ShowBalloonTip(3000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ConnectTest_Conf.txt"))
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ConnectTest_Conf.txt");
+                MessageBox.Show("설정을 초기화했습니다. 프로그램을 다시 시작해 주시면 적용됩니다.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else {
+                MessageBox.Show("설정이 존재하지 않습니다.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
